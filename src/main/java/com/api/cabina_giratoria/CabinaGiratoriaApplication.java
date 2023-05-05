3c8ec2945084f835ed9da2c411b2ded748ad9dbb
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-05-04 22:40:46 -0600

    Estructra del BackEnd API

---
+++ src/main/java/com/api/cabina_giratoria/CabinaGiratoriaApplication.java
@@ -0,0 +1,13 @@
+package com.api.cabina_giratoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinaGiratoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinaGiratoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/cabina_giratoria/CabinaGiratoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cabina_giratoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinaGiratoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
