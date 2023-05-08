9fce64b1dd451d984fe31909b947d1c5f8f8ac31
Edwin Alejandro <Edwin_Alejandro@gmail.com>
2023-05-08 17:07:20 -0600

    Mostrar los videos en el html

--- src/app/mostrar-fiesta/mostrar-fiesta.component.ts
+++ src/app/mostrar-fiesta/mostrar-fiesta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MostrarFiestaService } from './mostrar-fiesta.service';
 
@@ -11,7 +11,7 @@ export class MostrarFiestaComponent implements OnInit{
   url: any;
   id:any;
 
-  constructor(private router: ActivatedRoute, private mostrarFiestaService: MostrarFiestaService) { }
+  constructor(private router: ActivatedRoute, private mostrarFiestaService: MostrarFiestaService, private elementRef: ElementRef) { }
 
   ngOnInit(): void {
     this.id = this.router.snapshot.paramMap.get("id");
@@ -19,11 +19,27 @@ export class MostrarFiestaComponent implements OnInit{
     this.mostrarFiestaService.getInfo(this.id).subscribe(
       () => {
         this.url = this.mostrarFiestaService.respuesta;
-        console.log(this.url)
+        this.mostrarVideos();
       },
       (error) => {
         console.log(error);
       }
     );
+  }
+
+  mostrarVideos(): void {
+    const videosDiv = this.elementRef.nativeElement.querySelector('#videos');
+    let html = '';
+    for (const key in this.url) {
+      if (this.url.hasOwnProperty(key)) {
+        let videoUrl = this.url[key];
+        const safe = videoUrl.replace('?dl=0', '') + "?raw=1"
+
+        html += `<video width="320" height="240" controls>
+          <source src="${safe}" type="video/mp4">
+        </video>`;
+      }
+    }
+    videosDiv.innerHTML = html;
   }
 }
