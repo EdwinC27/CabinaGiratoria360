05a41ea28fed50d0c656ed6ca74b81b4b51e38c7
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-06-13 23:20:02 -0600

    Ejectar la union de los videos una vez que los otros 2 videos terminaran

--- app/src/main/java/com/example/cabinagiratoria/VideoConEfecto.java
+++ app/src/main/java/com/example/cabinagiratoria/VideoConEfecto.java
@@ -1,27 +1,46 @@
 package com.example.cabinagiratoria;
 
-import android.os.Handler;
 import android.util.Log;
 
 import com.arthenica.mobileffmpeg.ExecuteCallback;
 import com.arthenica.mobileffmpeg.FFmpeg;
 
+import java.util.concurrent.CompletableFuture;
+
 
 public class VideoConEfecto {
-    public static void realintizarVideo() {
-        ida();
-        vuelta();
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                unirVideos();
-            }
-        }, 50000); // 10000 milisegundos = 10 segundos
+    public static void realintizarVideo() {
+        ida().thenCompose((Void) -> vuelta())
+                .thenAccept((Void) -> {
+                    String videoIda = MP4Utils.getSelectedFileProcess();
+                    String videoRegreso = MP4Utils.getSelectedFileRever();
+                    String newArchivo = Video.createVideoFile("Final").getPath();
+
+                    String[] command = {"-y", "-i", videoIda, "-i", videoRegreso, "-filter_complex",
+                            "[0:v]scale=w=640:h=480:force_original_aspect_ratio=1[v0];[1:v]scale=w=640:h=480:force_original_aspect_ratio=1[v1];[v0][v1]concat=n=2:v=1[a]",
+                            "-map", "[a]", newArchivo};
+
+                    FFmpeg.executeAsync(command, new ExecuteCallback() {
+                        @Override
+                        public void apply(long executionId, int returnCode) {
+                            Log.d("Return Final: ", returnCode + ": ********************************************************");
+                            if (returnCode == 0) {
+                                MP4Utils.setSelectedFileFinal(newArchivo);
+                                MP4Utils.setSelectedFileWithAudio(VideoConMusica.cambiarVideoPorVideoConAudio());
+
+                            }
+                        }
+                    });                })
+                .exceptionally(ex -> {
+                    // Se ejecuta si ocurre algún error en ida() o vuelta()
+                    Log.d("Error en unirVideos(): ", ex.getMessage());
+                    return null;
+                });
     }
 
-    public static void ida() {
+    public static CompletableFuture<Void> ida() {
+        CompletableFuture<Void> future = new CompletableFuture<>();
         String video = MP4Utils.getSelectedFileVideo();
         String newArchivo = Video.createVideoFile("Ida").getPath();
 
@@ -39,12 +58,18 @@ public class VideoConEfecto {
                 Log.d("Return realintizarVideoDuplicado IDA: ", String.valueOf(returnCode));
                 if (returnCode == 0) {
                     MP4Utils.setSelectedFileProcess(newArchivo);
+                    future.complete(null); // Marcar el CompletableFuture como completado
+                } else {
+                    future.completeExceptionally(new RuntimeException("Error en ida()")); // Marcar el CompletableFuture como completado con error
                 }
             }
         });
+
+        return future;
     }
 
-    public static void vuelta() {
+    public static CompletableFuture<Void> vuelta() {
+        CompletableFuture<Void> future = new CompletableFuture<>();
         String video = MP4Utils.getSelectedFileVideo();
         String newArchivo = Video.createVideoFile("Regreso").getPath();
 
@@ -55,39 +80,20 @@ public class VideoConEfecto {
                         "[v1][v2][v3]concat=n=3:v=1:a=0,reverse[out]",
                 "-map", "[out]", newArchivo};
 
-
         FFmpeg.executeAsync(command, new ExecuteCallback() {
             @Override
             public void apply(long executionId, int returnCode) {
                 Log.d("Return realintizarVideoDuplicado Regreso: ", String.valueOf(returnCode));
                 if (returnCode == 0) {
                     MP4Utils.setSelectedFileRever(newArchivo);
+                    future.complete(null); // Marcar el CompletableFuture como completado
+                } else {
+                    future.completeExceptionally(new RuntimeException("Error en vuelta()")); // Marcar el CompletableFuture como completado con error
                 }
             }
         });
-    }
 
-    public static void unirVideos() {
-        String videoIda = MP4Utils.getSelectedFileProcess();
-        String videoRegreso = MP4Utils.getSelectedFileRever();
-        String newArchivo = Video.createVideoFile("Final").getPath();
-
-
-        String[] command = {"-y", "-i", videoIda, "-i", videoRegreso, "-filter_complex",
-                "[0:v]scale=w=640:h=480:force_original_aspect_ratio=1[v0];[1:v]scale=w=640:h=480:force_original_aspect_ratio=1[v1];[v0][v1]concat=n=2:v=1[a]",
-                "-map", "[a]", newArchivo};
-
-        FFmpeg.executeAsync(command, new ExecuteCallback() {
-            @Override
-            public void apply(long executionId, int returnCode) {
-                Log.d("Return Final: ", returnCode + ": ********************************************************");
-                if (returnCode == 0) {
-                    MP4Utils.setSelectedFileFinal(newArchivo);
-                    MP4Utils.setSelectedFileWithAudio(VideoConMusica.cambiarVideoPorVideoConAudio());
-
-                }
-            }
-        });
+        return future;
     }
 
 }
