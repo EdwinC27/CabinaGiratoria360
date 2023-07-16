521758b665446634652e23890927e1bfb29107d5
Edwin Castillo <mx.it192@astrazeneca.com>
2023-07-15 22:24:28 -0600

    Agregar una ventana donde se marca evento creado

--- src/app/crear-carpeta/crear-carpeta.component.ts
+++ src/app/crear-carpeta/crear-carpeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
 import { NombreFiestaService } from '../NombreFiesta/Nombre.Fiesta.Service'
 import { PeticionCrearCarpeta } from '../Peticiones-API/CrearCarpeta/crear';
@@ -14,12 +14,16 @@ export class CrearCarpetaComponent {
   responseData: any;
   mensajeEliminacion: string = "";
 
-  constructor(private nombreFiesta: NombreFiestaService, private router: Router, private peticionCrearCarpeta: PeticionCrearCarpeta, private cdr: ChangeDetectorRef) { }
+  isPopupVisible: boolean = false;
+
+  constructor(private nombreFiesta: NombreFiestaService, private router: Router, private peticionCrearCarpeta: PeticionCrearCarpeta, private cdr: ChangeDetectorRef,  private renderer: Renderer2) { }
 
   crear() {
     this.peticionCrearCarpeta.getCrearFiesta(this.textnombreFiesta).subscribe((message) => {
       this.responseData = message;
       if (this.responseData == "Carpeta creada correctamente") {
+        this.showPopup("Evento creado correctamente");
+
         this.mensajeEliminacion = "Evento creado correctamente";
         this.cdr.detectChanges(); // Detectar cambios para actualizar el HTML
       }
@@ -27,6 +31,34 @@ export class CrearCarpetaComponent {
       this.mensajeEliminacion = error;
       this.cdr.detectChanges(); // Detectar cambios para actualizar el HTML
     });
+  }
+
+  showPopup(message: string) {
+    const popupContainer = this.renderer.createElement('div');
+    const popupText = this.renderer.createText(message);
+
+    this.renderer.appendChild(popupContainer, popupText);
+    this.renderer.setAttribute(popupContainer, 'id', 'popupContainer');
+
+    this.renderer.listen(popupContainer, 'click', () => {
+      this.hidePopup(popupContainer);
+    });
+
+    this.renderer.appendChild(document.body, popupContainer);
+    this.cdr.detectChanges();
+
+    this.isPopupVisible = true;
+
+    setTimeout(() => {
+      this.hidePopup(popupContainer);
+    }, 5000);
+  }
+
+  hidePopup(popupContainer: any) {
+    this.renderer.removeChild(document.body, popupContainer);
+    this.cdr.detectChanges();
+
+    this.isPopupVisible = false;
   }
 
   regresar() {
