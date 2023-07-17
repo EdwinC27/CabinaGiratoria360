20b36b3c1d0aa07d580f1c3b12df564349c92d78
Edwin Castillo <mx.it192@astrazeneca.com>
2023-07-17 12:50:09 -0600

    Modificar la validacion de si la crapeta esta creada

--- src/main/java/com/api/cabina_giratoria/servicios/Validaciones.java
+++ src/main/java/com/api/cabina_giratoria/servicios/Validaciones.java
@@ -2,13 +2,9 @@ package com.api.cabina_giratoria.servicios;
 
 import com.amazonaws.services.s3.AmazonS3;
 
-import com.amazonaws.services.s3.model.ListObjectsV2Result;
-import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class Validaciones {
@@ -31,9 +27,6 @@ public class Validaciones {
     public boolean folderExists(String folderName) {
         String folderKey = folderName + "/"; // Agrega "/" al final para indicar que es una carpeta
 
-        ListObjectsV2Result result = amazonS3.listObjectsV2(bucketName, folderKey);
-        List<S3ObjectSummary> objects = result.getObjectSummaries();
-
-        return objects.isEmpty();
+        return amazonS3.doesObjectExist(bucketName, folderKey);
     }
 }
