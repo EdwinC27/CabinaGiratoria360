51636288cf0c74a43edfc268a1214c68e87992ab
EdwinC27 <edwin.a.castillo.27@gmail.com>
2023-09-03 16:09:43 -0600

    Modificar las rutas de a onde apunta la API y a la pagina publica

--- src/environments/environmet.ts
+++ src/environments/environmet.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  APIUrlBase: "http://3.128.181.152:8080/api/",
+  APIUrlBase: "http://3.133.85.94:8080/api/",
   APIUrlEliminarCarpeta: "carpeta/eliminar?carpeta=",
   APIUrlCrearCarpeta: "carpeta/crear?carpeta=",
   APIUrlTraerVideo: "videos?fiesta=",
@@ -12,7 +12,7 @@ export const environment = {
   APIUrlLogo: "logo/?usuario=",
 
   URLPaginaPublicaUsuario: "/usuario/",
-  URLPaginaPublica: "http://cabina-giratoria-publico.s3-website.us-east-2.amazonaws.com/#/evento/",
+  URLPaginaPublica: "http://cabina-giratoria.s3-website.us-east-2.amazonaws.com/#/evento/",
 
   ClaveDeCifrado: ""
 }
